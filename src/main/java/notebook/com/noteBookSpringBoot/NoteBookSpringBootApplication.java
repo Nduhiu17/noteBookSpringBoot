926b9ec5a1c67e355b418c3fff1a3a363ce7ce1a
nduhiu <nduhiu254@gmail.com>
2018-12-20 18:58:35 +0300

    initial commit

---
+++ src/main/java/notebook/com/noteBookSpringBoot/NoteBookSpringBootApplication.java
@@ -0,0 +1,14 @@
+package notebook.com.noteBookSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBookSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteBookSpringBootApplication.class, args);
+	}
+
+}
+
